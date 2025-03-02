622ea542188345e4f115ad42f24d10d8a85220d1
Dizari1 <165423301+Dizari1@users.noreply.github.com>
2025-03-02 21:06:21 +0300

    game.js

--- js/game.js
+++ js/game.js
@@ -1,95 +1,73 @@
-var cvs = document.getElementById("canvas");
-var ctx = cvs.getContext("2d");
+document.addEventListener("DOMContentLoaded", function () {
+    var cvs = document.getElementById("canvas");
+    var ctx = cvs.getContext("2d");
 
-var bird = new Image();
-var bg = new Image();
-var fg = new Image();
-var pipeUp = new Image();
-var pipeBottom = new Image();
+    function resizeCanvas() {
+        var scale = window.innerWidth / 288;
+        cvs.style.width = "100vw";
+        cvs.style.height = 512 * scale + "px";
+        document.body.style.overflow = "hidden";
+    }
+    resizeCanvas();
+    window.addEventListener("resize", resizeCanvas);
 
-bird.src = "img/bird.png";
-bg.src = "img/bg.png";
-fg.src = "img/fg.png";
-pipeUp.src = "img/pipeUp.png";
-pipeBottom.src = "img/pipeBottom.png";
+    var bird = new Image(),
+        bg = new Image(),
+        fg = new Image(),
+        pipeUp = new Image(),
+        pipeBottom = new Image();
+    
+    bird.src = "img/bird.png";
+    bg.src = "img/bg.png";
+    fg.src = "img/fg.png";
+    pipeUp.src = "img/pipeUp.png";
+    pipeBottom.src = "img/pipeBottom.png";
 
-// Звуковые файлы
-var fly = new Audio();
-var score_audio = new Audio();
+    var fly = new Audio(), score_audio = new Audio();
+    fly.src = "audio/fly.mp3";
+    score_audio.src = "audio/score.mp3";
 
-fly.src = "audio/fly.mp3";
-score_audio.src = "audio/score.mp3";
+    var gap = 90;
+    var xPos = 10, yPos = 150, grav = 1.2, velocity = 0;
+    var pipe = [{ x: cvs.width, y: 0 }];
+    var score = 0;
 
-var gap = 90;
-
-// Управление с клавиатуры
-document.addEventListener("keydown", moveUp);
-
-// Управление на сенсоре (касание)
-document.addEventListener("touchstart", moveUp);
-
-function moveUp() {
-    yPos -= 25;
-    fly.play();
-}
-
-// Создание труб
-var pipe = [];
-pipe[0] = {
-    x: cvs.width,
-    y: 0
-};
-
-var score = 0;
-
-// Позиция птички
-var xPos = 10;
-var yPos = 150;
-var grav = 1.5;
-
-function draw() {
-    ctx.drawImage(bg, 0, 0);
-
-    for (var i = 0; i < pipe.length; i++) {
-        ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
-        ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
-
-        pipe[i].x--;
-
-        if (pipe[i].x === 125) {
-            pipe.push({
-                x: cvs.width,
-                y: Math.floor(Math.random() * pipeUp.height) - pipeUp.height
-            });
-        }
-
-        // Проверка на столкновение
-        if (
-            xPos + bird.width >= pipe[i].x &&
-            xPos <= pipe[i].x + pipeUp.width &&
-            (yPos <= pipe[i].y + pipeUp.height ||
-                yPos + bird.height >= pipe[i].y + pipeUp.height + gap) ||
-            yPos + bird.height >= cvs.height - fg.height
-        ) {
-            location.reload(); // Перезагрузка страницы при проигрыше
-        }
-
-        if (pipe[i].x === 5) {
-            score++;
-            score_audio.play();
-        }
+    function moveUp() {
+        velocity = -4.5;
+        fly.play();
     }
 
-    ctx.drawImage(fg, 0, cvs.height - fg.height);
-    ctx.drawImage(bird, xPos, yPos);
+    document.addEventListener("keydown", function (event) {
+        if (event.code === "Space") moveUp();
+    });
+    document.addEventListener("touchstart", moveUp);
 
-    yPos += grav;
+    function draw() {
+        ctx.drawImage(bg, 0, 0);
+        for (var i = 0; i < pipe.length; i++) {
+            ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
+            ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
+            pipe[i].x--;
+            if (pipe[i].x === 125) {
+                pipe.push({ x: cvs.width, y: Math.floor(Math.random() * pipeUp.height) - pipeUp.height });
+            }
+            if (xPos + bird.width >= pipe[i].x && xPos <= pipe[i].x + pipeUp.width && (yPos <= pipe[i].y + pipeUp.height || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height) {
+                location.reload();
+            }
+            if (pipe[i].x === 5) {
+                score++;
+                score_audio.play();
+            }
+        }
+        ctx.drawImage(fg, 0, cvs.height - fg.height);
+        ctx.drawImage(bird, xPos, yPos);
+        velocity += grav;
+        yPos += velocity;
 
-    ctx.fillStyle = "#000";
-    ctx.font = "24px Verdana";
-    ctx.fillText("Счет: " + score, 10, cvs.height - 20);
-
-    requestAnimationFrame(draw);
-}
-
-pipeBottom.onload = draw;
+        ctx.fillStyle = "#000";
+        ctx.font = "24px Verdana";
+        ctx.fillText("Счет: " + score, 10, cvs.height - 20);
+        requestAnimationFrame(draw);
+    }
+    pipeBottom.onload = draw;
+});
